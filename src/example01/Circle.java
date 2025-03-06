cb26b622ac50cb1dd01901e9fe81e117ed63d46c
Shohruh <sharipovshohruh@gmail.com>
2025-03-06 23:11:03 +0500

    java-oop-exercises

---
+++ src/example01/Circle.java
@@ -0,0 +1,24 @@
+package example01;
+
+class Circle {
+    double radius;
+    String color;
+
+    Circle(){
+        radius=1.0;
+        color="red";
+    }
+
+    Circle(double r){
+        radius=r;
+        color="red";
+    }
+
+    double getRadius(){
+        return  radius;
+    }
+
+    double getArea(){
+        return  radius*radius*Math.PI;
+    }
+}
