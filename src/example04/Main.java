7d23f10eb55e0f9c275095457b97efe51d20dba4
Shohruh <sharipovshohruh@gmail.com>
2025-03-07 02:17:21 +0500

    java-oop-exercises

---
+++ src/example04/Main.java
@@ -0,0 +1,18 @@
+package example04;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1=new Employee(8,"Pater","Tan",2500);
+        System.out.println(e1);
+        e1.setSalary(999);
+        System.out.println(e1);
+        System.out.println(e1.getID());
+        System.out.println(e1.getFirstName());
+        System.out.println(e1.getLastName());
+        System.out.println(e1.getSalary());
+        System.out.println(e1.getName());
+        System.out.println(e1.getAnnualSalary());
+        System.out.println(e1.raisSalary(10));
+        System.out.println(e1);
+    }
+}
