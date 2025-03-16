7aba8044919ba0a08650104af32bf38d8884c824
Shohruh <sharipovshohruh@gmail.com>
2025-03-17 00:01:54 +0500

    java-oop-exercises

--- src/oop5/example01/Circle.java
+++ src/oop5/example01/Circle.java
@@ -1,5 +1,48 @@
 package oop5.example01;
 
 public class Circle {
+    private double radius;
+    private String color;
 
+    Circle(){
+        radius=1.0;
+        color="Red";
+    }
+
+    Circle(double radius){
+        this.radius=radius;
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    double getArea(){
+        return Math.PI*Math.pow(radius,2);
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
--- src/oop5/example01/Cylinder.java
+++ src/oop5/example01/Cylinder.java
@@ -1,5 +1,28 @@
 package oop5.example01;
 
-public class Cylinder {
+public class Cylinder extends Circle {
+    private double height;
 
+    Cylinder(){
+        super();
+        height=1.0;
+    }
+
+    public Cylinder(double height) {
+        super();
+        this.height = height;
+    }
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return  getArea()*height;
+    }
 }
